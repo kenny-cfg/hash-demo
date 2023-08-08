aa0cd4dc0a541b9acbe298d8f183fa2c70a156c5
Kenny Hung (he) <kenny.cfg@outlook.com>
2023-08-08 20:02:20 +0100

    Working password store

--- password_store.py
+++ password_store.py
@@ -1,2 +1,9 @@
 class PasswordStore:
-    pass
+    def __init__(self):
+        self.username_passwords = {}
+
+    def add(self, username, password):
+        self.username_passwords[username] = password
+
+    def verify(self, username, password):
+        return username in self.username_passwords and self.username_passwords[username] == password
